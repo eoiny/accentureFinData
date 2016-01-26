e89958ce31de266340dd46bcfa9d272448edf5bc
Eoin Ó Loideáin <eoloideain@gmail.com>
2016-01-26 23:38:47 +0000

    add treemap

--- js/treemap.js
+++ js/treemap.js
@@ -90,5 +90,87 @@ function treemap(){
       ]
   };
 
+  var margin = {top:40, right:10, left:10, bottom:40},
+    width = 760 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+ /* var dataScale = d3.scale.linear()
+    .domain([d3.min(data1, function(d){return d.revenue}),
+      d3.max(data1, function(d){return d.revenue})]);
+
+  dataScale.range([0,100]);*/
+
+
+  var treemap = d3.layout.treemap()
+    .size([width, height])
+    .sticky(false)
+    //.sort(function(a,b) { return a.revenue - b.revenue; })
+    .round(true)
+    .value(function(d) { return d.revenue; });
+
+  var div = d3.select("#treemap").append("div")
+    .style("position", "relative")
+    .style("width", (width + margin.left + margin.right) + "px")
+    .style("height", (height + margin.top + margin.bottom) + "px")
+    .style("left", margin.left + "px")
+    .style("top", margin.top + "px")
+    .attr("id", "first");
+
+  var node = div.datum(data1).selectAll(".node")
+    .data(treemap.nodes)
+    .enter().append("div")
+    .attr("class", "node")
+    .call(position)
+    .style("background", function(d) { return d.color ? d.color : "#ffffff"; })
+    .text(function(d) { return d.children ? "blue" : d.name + "(" + d.revenue + ")"; });
+
+  d3.select("#revenue1").on("click", function() {
+    var node = div.datum(data1).selectAll(".node")
+      .data(treemap.nodes);
+    node.enter().append("div")
+      .attr("class", "node");
+    node.exit().remove();
+
+    node.transition().duration(1500).call(position)
+      .style("background", function(d) { return d.color ? d.color : "#ffffff"; })
+      .text(function(d) { return d.children ? "blue" : d.name + "(" + d.revenue + ")"; });
+
+  });
+  d3.select("#revenue2").on("click", function() {
+    var node = div.datum(data2).selectAll(".node")
+      .data(treemap.nodes);
+    node.enter().append("div")
+      .attr("class", "node");
+    node.exit().remove();
+
+    node.transition().duration(1500).call(position)
+      .style("background", function(d) { return d.color ? d.color : "#ffffff"; })
+      .text(function(d) { return d.children ? "blue" : d.name + "(" + d.revenue + ")"; });
+
+  });
+  d3.select("#revenue3").on("click", function() {
+    var node = div.datum(data3).selectAll(".node")
+      .data(treemap.nodes);
+    node.enter().append("div")
+      .attr("class", "node");
+    node.exit().remove();
+
+    node.transition().duration(1500).call(position)
+      .style("background", function(d) { return d.color ? d.color : "#ffffff"; })
+      .text(function(d) { return d.children ? "blue" : d.name + "(" + d.revenue + ")"; });
+
+  });
+
 
 }
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+    .style("top", function(d) { return d.y + "px"; })
+    .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+    .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
+
+
+treemap();
+
