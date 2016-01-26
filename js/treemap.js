502d3630fcf9fd099dfd6d4566804fcc71d90e5a
Eoin Ó Loideáin <eoloideain@gmail.com>
2016-01-26 21:28:05 +0000

    data setup

---
+++ js/treemap.js
@@ -0,0 +1,94 @@
+function treemap(){
+
+  var data1 = {
+    "name": "items",
+    "children"
+:
+  [
+    {
+      "id": "1",
+      "revenue": 16203915,
+      "color": "#ff8080",
+      "name": "Consulting"
+    },
+    {
+      "id": "2",
+      "revenue": 14844016,
+      "color": "#80ff80",
+      "name": "Outsourcing"
+    }
+  ]
+};
+
+  var data2 = {
+    "name": "items",
+    "children"
+      :
+      [
+        {
+          "id": "1",
+          "revenue": 14209387,
+          "color": "#008080",
+          "name": "North America"
+        },
+        {
+          "id": "2",
+          "revenue": 14844016,
+          "color": "#800080",
+          "name": "Europe"
+        },
+        {
+          "id": "3",
+          "revenue": 5950595,
+          "color": "#808000",
+          "name": "Growth markets"
+        }
+      ]
+  };
+
+  var data3 = {
+    "name": "items",
+    "children"
+      :
+      [
+        {
+        "id": "1",
+        "revenue": 6349372,
+        "color": "#d73027",
+        "name": "Communication, Media & Technology"
+      },
+        {
+          "id": "2",
+          "revenue": 6634771,
+          "color": "#fc8d59",
+          "name": "Financial Services"
+        },
+        {
+          "id": "3",
+          "revenue": 5462550,
+          "color": "#fee090",
+          "name": "Health & Public Services"
+        },
+        {
+          "id": "4",
+          "revenue": 7596051,
+          "color": "#e0f3f8",
+          "name": "Products"
+        },
+        {
+          "id": "5",
+          "revenue": 4988627,
+          "color": "#91bfdb",
+          "name": "Resources"
+        },
+        {
+          "id": "6",
+          "revenue": 16560,
+          "color": "#4575b4",
+          "name": "Other"
+        }
+      ]
+  };
+
+
+}
